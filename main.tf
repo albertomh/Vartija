6b1da6d719ec8978340d5bf05c9a42fa24eb4f69
Alberto Morón Hernández <alberto.moronh@gmail.com>
2022-08-19 22:08:41 +0100

    Create Docker image and push to ECR

--- main.tf
+++ main.tf
@@ -41,3 +41,27 @@ locals {
 resource aws_ecr_repository repo {
   name = local.ecr_repository_name
 }
+
+resource null_resource ecr_image {
+  triggers = {
+    python_file = md5(file("${path.module}/vartija/main.py"))
+    docker_file = md5(file("${path.module}/_docker/dev/Dockerfile.vartija.dev"))
+  }
+
+  provisioner "local-exec" {
+  command = <<EOF
+  aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${local.account_id}.dkr.ecr.${var.region}.amazonaws.com
+  cd ${path.module}/_docker/dev/
+  docker build -t ${aws_ecr_repository.repo.repository_url}:${local.ecr_image_tag} -f Dockerfile.vartija.dev ../..
+  docker push ${aws_ecr_repository.repo.repository_url}:${local.ecr_image_tag}
+EOF
+  }
+}
+
+data aws_ecr_image lambda_image {
+  depends_on = [
+    null_resource.ecr_image
+  ]
+  repository_name = local.ecr_repository_name
+  image_tag       = local.ecr_image_tag
+}
