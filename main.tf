17cd9829d5c28e1d70f6c80d3baa62497ec5059a
Alberto Morón Hernández <alberto.moronh@gmail.com>
2022-08-19 22:08:25 +0100

    Create ECR repository

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 /**
-Vartija Terraform configuration
+Vartija Terraform template
 Copyright 2022 Alberto Morón Hernández
 
 ■ Table of Contents ■
   SETUP
+  ECR
 
 ═════════════════════════════════════════════════════════════════════════════ */
 
@@ -25,4 +26,18 @@ terraform {
 provider aws {
   profile = "default"
   region  = var.region
+}
+
+data aws_caller_identity current {}
+
+locals {
+  account_id          = data.aws_caller_identity.current.account_id
+  ecr_repository_name = "vartija"
+  ecr_image_tag       = "latest"
+}
+
+//  ECR ────────────────────────────────────────────────────────────────────────
+
+resource aws_ecr_repository repo {
+  name = local.ecr_repository_name
 }
